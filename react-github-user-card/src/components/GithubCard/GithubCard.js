8adeebb048c94c8ef6876e9c48dd1dbe8b776542
Victor Tran <victor.n.tran@outlook.com>
2019-11-09 13:04:07 -0800

    create individual card, and the list of card components

--- react-github-user-card/src/components/GithubCard/GithubCard.js
+++ react-github-user-card/src/components/GithubCard/GithubCard.js
@@ -1,26 +1,32 @@
-import React from 'react'
+import React from "react";
 
-const GithubCard = ({avatar_url, name, location, bio, login, html_url}) => {
+const GithubCard = ({ avatar_url, name, location, bio, login, html_url }) => {
   return (
     // Card component
     <section className="card">
-      <img
-        src={avatar_url}
-        alt="github avatar"
-        className="card-img-top"
-      />
+      <img src={avatar_url} alt="github avatar" className="card-img-top" />
       <section className="name card-text">
+        {name ? (
+          `${name}` && (
+            <>
+              <a href={html_url} className="card-text">
+                (@{login})
+              </a>
+            </>
+          )
+        ) : (
+          <a href={html_url} className="card-text">
+            {login}
+          </a>
+        )}
         {name}
-        <a href={html_url} className="card-text">
-          (@{login})
-        </a>
       </section>
       <section className="location card-text">
-        Located: {location}
+        {location ? `Located: ${location}` : null}
       </section>
-      <section className="bio card-text">Bio: {bio}</section>
+      <section className="bio card-text">{bio ? `Bio: ${bio}` : null}</section>
     </section>
-  )
-}
+  );
+};
 
-export default GithubCard
+export default GithubCard;
